86d47a8bc437c43da6f3d517b3634cbcc873c0f1
Lucas Gertsch <lucas.gertsch@ffwagency.com>
2017-07-04 16:24:12 +0200

    [task1a1b] added imdb_movie_info_crwaler module

---
+++ drupal/web/sites/all/modules/costum/imdb movie info crawler/templates/movie_infos.tpl.php
@@ -0,0 +1,10 @@
+<h1><?php print $infos['title']; ?></h1>
+<div>
+    <a href="<?php print $infos['posterlink']; ?>" >
+           <img alt='movie poster'  src="<?php print $infos['postersrc']; ?>"/>
+    </a>
+</div>
+</br>
+<p><?php print $infos['plot']; ?></p>
+</br>
+<h1><a style="color: red; text-decoration: underline; font-size: 200%;" href='./Imdb_Crawler'>again</a></h1>
