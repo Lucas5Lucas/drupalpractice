363f53ef06968b15988dc09c65e64a506f6150b8
Lucas Gertsch <lucas.gertsch@ffwagency.com>
2017-07-11 11:01:17 +0200

    [task1a1b] added t() functions and remioved private.xml from index

--- drupal/web/sites/all/modules/costum/imdb movie info crawler/templates/movie_infos.tpl.php
+++ drupal/web/sites/all/modules/costum/imdb movie info crawler/templates/movie_infos.tpl.php
@@ -1,4 +1,4 @@
-<h1><?php print !empty($infos['title']) ? $infos['title'] : "No title found!" ; ?></h1>
+<h1><?php print !empty($infos['title']) ? $infos['title'] : t("No title found!") ; ?></h1>
 <?php if (!empty($infos['postersrc'])): ?>
     <div>
         <a href="<?php print $infos['posterlink']; ?>" >
@@ -9,4 +9,4 @@
 </br>
 <p><?php print !empty($infos['plot']) ? $infos['plot'] : "" ; ?></p>
 </br>
-<h1><?php print l("AGAIN", "/imdb_crawler", array('attributes' => array('class' => 'back-link'))); ?></h1>
+<h1><?php print l(t("AGAIN"), "/imdb_crawler", array('attributes' => array('class' => 'back-link'))); ?></h1>
