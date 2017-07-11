885afc1bba63d6c8d89080a6697675e281589ccb
Lucas Gertsch <lucas.gertsch@ffwagency.com>
2017-07-11 10:18:47 +0200

    [task1a1b] made some more changes

--- drupal/web/sites/all/modules/costum/imdb movie info crawler/templates/movie_infos.tpl.php
+++ drupal/web/sites/all/modules/costum/imdb movie info crawler/templates/movie_infos.tpl.php
@@ -1,10 +1,12 @@
-<h1><?php print $infos['title']; ?></h1>
-<div>
-    <a href="<?php print $infos['posterlink']; ?>" >
-           <img alt='movie poster'  src="<?php print $infos['postersrc']; ?>"/>
-    </a>
-</div>
+<h1><?php print !empty($infos['title']) ? $infos['title'] : "No title found!" ; ?></h1>
+<?php if (!empty($infos['postersrc'])): ?>
+    <div>
+        <a href="<?php print $infos['posterlink']; ?>" >
+            <img alt='movie poster'  src="<?php print print !empty($infos['postersrc']) ? $infos['postersrc'] : "" ; ?>"/>
+        </a>
+    </div>
+<?php endif; ?>    
 </br>
-<p><?php print $infos['plot']; ?></p>
+<p><?php print !empty($infos['plot']) ? $infos['plot'] : "" ; ?></p>
 </br>
-<h1><a style="color: red; text-decoration: underline; font-size: 200%;" href='./Imdb_Crawler'>again</a></h1>
+<h1><?php print l("AGAIN", "/imdb_crawler", array('attributes' => array('class' => 'back-link'))); ?></h1>
