62e1123e843892f54a693d26e6cc879d38394652
Lucas Gertsch <lucas.gertsch@ffwagency.com>
2017-07-17 17:52:29 +0200

    [task3a3b] formatted the code

--- drupal/web/sites/all/modules/costum/upcoming movie list/templates/upcoming_movie_list_only_titles.tpl.php
+++ drupal/web/sites/all/modules/costum/upcoming movie list/templates/upcoming_movie_list_only_titles.tpl.php
@@ -1,7 +1,8 @@
- <ol>
-<?php foreach ($movietitles as $title){
-    print "<h2><li>".$title."</li></h2>";
-}
-?>   
+<ol>
+    <?php
+    foreach ($movietitles as $title) {
+        print "<h2><li>" . $title . "</li></h2>";
+    }
+    ?>   
 </ol>
 <h3><?php print "Last updated: ".$time_of_update_markup;?></li></h3>
