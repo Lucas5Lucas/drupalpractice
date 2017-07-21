fcd25ca1e255b85cac973e39819152a9596c51bd
Lucas Gertsch <lucas.gertsch@ffwagency.com>
2017-07-21 13:45:10 +0200

    [task3a3b] updated module

--- drupal/web/sites/all/modules/costum/upcoming movie list/templates/upcoming_movie_list_only_titles.tpl.php
+++
@@ -1,8 +0,0 @@
-<ol>
-    <?php
-    foreach ($movietitles as $title) {
-        print "<li><h2>" . $title . "</h2></li>";
-    }
-    ?>   
-</ol>
-<h3><?php print "Last updated: ".$time_of_update_markup;?></h3>
